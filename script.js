e06a2cae79df7a1e16306d7e7fe0d566c69fdb89
kaliraj.am <kaliraj.am@zohocorp.com>
2025-02-28 13:35:00 +0530

    feat: Add dynamic content to the webpage

---
+++ script.js
@@ -0,0 +1,17 @@
+function showMessage() {
+    document.getElementById("message").innerText = "Hello, thanks for visiting!";
+}
+function updateContent() {
+    const contents = ["kali", "raj", "sri","ram","an","war"]; // Add more as needed
+    let index = 0;
+
+    setInterval(() => {
+        if(index == 0||index==2||index==4){
+            document.getElementById("dynamic-content").innerText = contents[index]+contents[index+1];
+        }else{
+            document.getElementById("dynamic-content").innerText = contents[index];
+        }
+        index = (index + 1) % contents.length; // Loop through array
+    }, 500);
+}
+updateContent();
